37d120720306707a472f7e7140e0b7faad851248
fr3d100 <f.bonnand@gmail.com>
2019-01-14 21:36:23 +0100

    Exo 6 fait

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -18,35 +18,41 @@ if word.start_with?("qu")
 
 elsif
 
-  #translates a word beginning with a vowel
+  #On cherche si le mot commence par une voyelle
     if word[0] =~ /^[aeiouy]/ 
+      # Et si oui on ajoute "ay"
         word << "ay" 
 
-  #translates a word beginning with a consonant
+  #Sinon c'est que le mot commence par une consonne 
     elsif  
-      
+      # On test la 2eme lettre
+          # Cas ou la deuxieme lettre est une voyelle
           if word[1] =~ /^[aeiouy]/  then
           word[0]= ""
           word << "#{word1}ay" 
 
-  #translates a word beginning with two consonants
+          #Sinon c'est uen consonne
           elsif
+              # Cas ou le mot commence par "qu"
               if word[1..2] == ("qu")
               word[0]= ""
               word[0]= ""
               word[0]= ""
+              # On va passer ce qu'on a retiré au début à la fin du mot
               word << "#{word1}#{word2}#{word3}ay"
 
               elsif word[2] =~ /^[aeiouy]/ then
               word[0]= ""
               word[0]= ""
+              # On va passer ce qu'on a retiré au début à la fin du mot
               word << "#{word1}#{word2}ay"
 
-  #translates a word beginning with three consonants
+              #sinon le mot commence avec 3 consonnes
               elsif
               word[0]= ""
               word[0]= ""
               word[0]= ""
+              # On va passer ce qu'on a retiré au début à la fin du mot
               word << "#{word1}#{word2}#{word3}ay" 
               end
 
@@ -59,6 +65,6 @@ elsif
 
 end
 
-
+# On refusionne les éléments du tableau
 return n.join(" ")
 end
