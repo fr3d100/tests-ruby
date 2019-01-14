ab599ee24c51c67e2f53c24d8318d30a4a22c6f3
fr3d100 <f.bonnand@gmail.com>
2019-01-14 07:31:13 +0100

    Exo 01 improvments

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,25 +1,32 @@
 def ftoc(temp)
-	if temp == 32
-		0
-	elsif temp == 212
-		100
-	elsif temp ==98.6
-		37
-	elsif temp==68
-		20
-	end
+	# 1) Gestion ds cas écris dans les tests 1 par 1
+		# if temp == 32
+		# 	0
+		# elsif temp == 212
+		# 	100
+		# elsif temp ==98.6
+		# 	37
+		# elsif temp==68
+		# 	20
+		# end
+
+	# 2) Gestion de n'importe quel cas en appliquant la formule de conversion
+	(temp-32)*5/9
 end
 
 
 def ctof(temp)
-	if temp == 0
-		32
-	elsif temp == 100
-		212
-	elsif temp == 37
-		98.6
-	elsif temp == 20
-		68
-	end
+	# 1) Gestion ds cas écris dans les tests 1 par 1
+		# if temp == 0
+		# 	32
+		# elsif temp == 100
+		# 	212
+		# elsif temp == 37
+		# 	98.6
+		# elsif temp == 20
+		# 	68
+		# end
 
+	# 2) Gestion de n'importe quel cas en appliquant la formule de conversion
+	(temp*1.8) + 32
 end
